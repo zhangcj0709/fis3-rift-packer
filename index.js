4b1ae8709ea2b13f3bc88f8c0b573c7e6a33c315
zhangcj <zcj@bankeroad.com>
2016-01-16 17:19:18 +0800

    打包时不收集script标签，页面依赖除外

--- index.js
+++ index.js
@@ -143,7 +143,9 @@ fis.on('compile:start', function(file) {
 fis.match('::package', {
     // npm install [-g] fis3-postpackager-loader
     // 分析 __RESOURCE_MAP__ 结构，来解决资源加载问题
-    postpackager: fis.plugin('loader')
+    postpackager: fis.plugin('loader', {
+        obtainScript: false
+    })
 });
 
 
